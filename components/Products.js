5673a1a56bc7e6bfe3e92fe75f4a15251051a66b
shanewmurphy <shn.murphy@gmail.com>
2022-09-17 04:18:55 +0100

    added custom react hook to manage cart state

--- components/Products.js
+++ components/Products.js
@@ -5,60 +5,15 @@ import Image from "next/image";
 import { initiateCheckout } from "lib/payments";
 import products from "@/data/products.json";
 
+import useCart from "@/hooks/use-cart";
+
 const defaultCart = {
   products: {},
 };
 
 export default function Productlist() {
-  const [cart, updateCart] = useState(defaultCart);
+  const { subtotal, totalItems, addToCart, checkout } = useCart();
 
-  const cartItems = Object.keys(cart.products).map((key) => {
-    const product = products.find(({ id }) => `${id}` === `${key}`);
-    return {
-      ...cart.products[key],
-      pricePerItem: product.price,
-    };
-  });
-
-  // calculate subtotal of items
-  const subtotal = cartItems.reduce(
-    (accumulator, { pricePerItem, quantity }) => {
-      return accumulator + pricePerItem * quantity;
-    },
-    0
-  );
-
-  console.log("subtotal", subtotal);
-
-  const totalItems = cartItems.reduce((accumulator, { quantity }) => {
-    return accumulator + quantity;
-  }, 0);
-
-  function addToCart({ id } = {}) {
-    updateCart((prev) => {
-      let cartState = { ...prev };
-
-      if (cartState.products[id]) {
-        cartState.products[id].quantity = cartState.products[id].quantity + 1;
-      } else {
-        cartState.products[id] = {
-          id,
-          quantity: 1,
-        };
-      }
-      return cartState;
-    });
-  }
-  function checkout() {
-    initiateCheckout({
-      lineItems: cartItems.map((item) => {
-        return {
-          price: item.id,
-          quantity: item.quantity,
-        };
-      }),
-    });
-  }
   return (
     <div>
       <div className={styles.products_grid_container}>
---
+++ hooks/use-cart.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { initiateCheckout } from "lib/payments";
+import products from "@/data/products.json";
+const defaultCart = {
+  products: {},
+};
+
+export default function useCart() {
+  const [cart, updateCart] = useState(defaultCart);
+  const cartItems = Object.keys(cart.products).map((key) => {
+    const product = products.find(({ id }) => `${id}` === `${key}`);
+    return {
+      ...cart.products[key],
+      pricePerItem: product.price,
+    };
+  });
+
+  // calculate subtotal of items
+  const subtotal = cartItems.reduce(
+    (accumulator, { pricePerItem, quantity }) => {
+      return accumulator + pricePerItem * quantity;
+    },
+    0
+  );
+
+  const totalItems = cartItems.reduce((accumulator, { quantity }) => {
+    return accumulator + quantity;
+  }, 0);
+
+  function addToCart({ id } = {}) {
+    updateCart((prev) => {
+      let cart = { ...prev };
+
+      if (cart.products[id]) {
+        cart.products[id].quantity = cart.products[id].quantity + 1;
+      } else {
+        cart.products[id] = {
+          id,
+          quantity: 1,
+        };
+      }
+      return cart;
+    });
+  }
+  function checkout() {
+    initiateCheckout({
+      lineItems: cartItems.map((item) => {
+        return {
+          price: item.id,
+          quantity: item.quantity,
+        };
+      }),
+    });
+  }
+  return {
+    cart,
+    updateCart,
+    subtotal,
+    totalItems,
+    addToCart,
+    checkout,
+  };
+}
