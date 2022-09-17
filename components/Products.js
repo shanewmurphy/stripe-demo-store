d52bc5721cc97c08304fedce85b83947c008b8e1
shanewmurphy <shn.murphy@gmail.com>
2022-09-17 03:58:25 +0100

    reverted back code before using useState hook logic in navigation menu

--- components/Products.js
+++ components/Products.js
@@ -2,13 +2,63 @@ import { useState } from "react";
 import styles from "./products.module.css";
 import Image from "next/image";
 
-import useCart from "@/hooks/use-cart.js";
-
+import { initiateCheckout } from "lib/payments";
 import products from "@/data/products.json";
 
-export default function Productlist() {
-  const { subtotal, totalItems, addToCart, checkout } = useCart();
+const defaultCart = {
+  products: {},
+};
 
+export default function Productlist() {
+  const [cart, updateCart] = useState(defaultCart);
+
+  const cartItems = Object.keys(cart.products).map((key) => {
+    const product = products.find(({ id }) => `${id}` === `${key}`);
+    return {
+      ...cart.products[key],
+      pricePerItem: product.price,
+    };
+  });
+
+  // calculate subtotal of items
+  const subtotal = cartItems.reduce(
+    (accumulator, { pricePerItem, quantity }) => {
+      return accumulator + pricePerItem * quantity;
+    },
+    0
+  );
+
+  console.log("subtotal", subtotal);
+
+  const totalItems = cartItems.reduce((accumulator, { quantity }) => {
+    return accumulator + quantity;
+  }, 0);
+
+  function addToCart({ id } = {}) {
+    updateCart((prev) => {
+      let cartState = { ...prev };
+
+      if (cartState.products[id]) {
+        cartState.products[id].quantity = cartState.products[id].quantity + 1;
+      } else {
+        cartState.products[id] = {
+          id,
+          quantity: 1,
+        };
+      }
+      return cartState;
+    });
+  }
+  function checkout() {
+    initiateCheckout({
+      lineItems: cartItems.map((item) => {
+        return {
+          price: item.id,
+          quantity: item.quantity,
+        };
+      }),
+    });
+  }
   return (
     <div>
       <div className={styles.products_grid_container}>
--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,7 @@
 import "../styles/globals.css";
 
-import { CartContext } from "@/hooks/use-cart.js";
-
-import Nav from "@/components/Nav";
-
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Nav />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
